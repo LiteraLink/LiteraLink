1740480988038c97eb74abf0f5be8e68ef6b9050
MartinTarigan <martin.marcelino@ui.ac.id>
2023-10-12 20:08:54 +0700

    Implemented fetching data from external API and saving to the database

--- main/models.py
+++ main/models.py
@@ -1,31 +1,12 @@
 from django.db import models
 
 class Book(models.Model):
-    kind = models.CharField(max_length=255)
-    google_id = models.CharField(max_length=255)  # Rename 'id' field to 'google_id' or another suitable name
-    etag = models.CharField(max_length=255)
-    selfLink = models.URLField()
+    bookID = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
-    authors = models.TextField()
-    publisher = models.CharField(max_length=255)
-    publishedDate = models.DateField()
+    authors = models.CharField(max_length=255)
     description = models.TextField()
-    textReadingMode = models.BooleanField()
-    imageReadingMode = models.BooleanField()
-    pageCount = models.PositiveIntegerField()
-    printType = models.CharField(max_length=255)
-    categories = models.TextField()
-    maturityRating = models.CharField(max_length=255)
-    allowAnonLogging = models.BooleanField()
-    contentVersion = models.CharField(max_length=255)
-    containsEpubBubbles = models.BooleanField()
-    containsImageBubbles = models.BooleanField()
-    smallThumbnail = models.URLField()
-    thumbnail = models.URLField()
-    language = models.CharField(max_length=255)
-    previewLink = models.URLField()
-    infoLink = models.URLField()
-    canonicalVolumeLink = models.URLField()
+    categories = models.CharField(max_length=255)
+    thumbnail = models.TextField()
 
     def __str__(self):
         return self.title
--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('fetch_api_data/', views.seeding_data, name='fetch_api_data'),
+    path('show_json/', views.show_json, name='show_json'),
 ]
