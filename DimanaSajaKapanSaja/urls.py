f89430a8d99e28838723a9cda69068dd58c02d01
MartinTarigan <martin.marcelino@ui.ac.id>
2023-10-28 10:24:08 +0700

    CRUD AJAX done

--- DimanaSajaKapanSaja/urls.py
+++ DimanaSajaKapanSaja/urls.py
@@ -7,10 +7,13 @@ app_name = 'dimanasajakapansaja'
 
 urlpatterns = [
     path('', views.show_station, name="show_station"),
-    # path('distribute/<int:station_id>/', views.distribute_book, name="distribute"),
     path('flush/', views.flush, name='flush'),
     path('add_station/', views.add_station, name='add_station'),
     path('del_station/<int:station_id>/', views.del_station, name='del_station'),
     path('edit_station/<int:station_id>/', views.edit_station, name='edit_station'),
     path('detail/<int:station_id>/', views.show_station_detail, name='detail'),
+    path('rent_book/<int:book_id>/', views.rent_book, name='rent_book'),
+    path('return_book/<int:station_id>/<int:book_id>/', views.return_book, name='return_book'),
+    path('get-station/', views.get_station_json, name='get_station_json'),
+    path('add-station-ajax/', views.add_station_ajax, name='add_station_ajax')
 ]
